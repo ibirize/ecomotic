e784619cbd2f5d929397a8eb9b33ea9e0b2f4207
Hector <hector.gallardo@alumni.mondragon.edu>
2017-06-09 16:14:17 +0200

    valores todos zigbee actualizados en variables luxometro, anemometro, lm35
    Signed-off-by: Hector <hector.gallardo@alumni.mondragon.edu>

--- controlSensores.py
+++ controlSensores.py
@@ -3,37 +3,38 @@ from controlPaneles import controlPaneles
 
 class controlSensores:
 
-    LM35=15
-    LUXOMETRO=25
-    ANEMOMETRO=35
+    LM35="0x13a200400a42f6"
+    LUXOMETRO="0x13a200406a8aa3"
+    ANEMOMETRO="0x13a200406a8ace"
     TEMPERATURA_MINIMA=18
     TEMPERATURA_MAXIMA=23
     mV_A_Grados = 10
     VELOCIDAD_VIENTO_MAXIMA = 30
-
+    valorLuxes = 0
+    valorTemperatura = 0
 
     def control(self):
 
         serial = False
-       # while (serial != True):
+        while (serial != True):
 
-        try:
+            try:
 
-            xbee = myXbee(9600)
+                xbee = myXbee(9600)
 
-            #message = xbee.recibir()
-            message=b'hola'
-            print(message)
-            print(xbee)
-            serial = True
+                message = xbee.recibir()
+                message=b'hola'
+                print(message)
+                print(xbee)
+                serial = True
 
-            print(xbee.frame2adcvalue(message))
+                print(xbee.frame2adcvalue(message))
 
-            print(xbee.getFrameSource(message))
+                print(xbee.getFrameSource(message))
 
 
-        except Exception as excepcion:
-                print(excepcion.args)
+            except Exception as excepcion:
+                    print(excepcion.args)
 
         while(True):
             mensajeRecibido = xbee.recibir()
@@ -41,17 +42,20 @@ class controlSensores:
             origenMensaje = xbee.getFrameSource(mensajeRecibido)
 
             if(origenMensaje==self.LM35):
-                self.temperatura(xbee, mensajeRecibido)
+                valorTemperatura = xbee.frame2adcvalue(mensajeRecibido) / self.mV_A_Grados
+
+                self.temperatura(self, valorTemperatura)
 
             if(origenMensaje==self.LUXOMETRO):
-               self.luxometro(xbee, mensajeRecibido)
+                valorLuxes = xbee.frame2adcvalue(mensajeRecibido)
+                self.luxometro(self, valorLuxes)
 
             if(origenMensaje==self.ANEMOMETRO):
-                self.anemometro(xbee, mensajeRecibido)
+                viento = xbee.frame2adcvalue(mensajeRecibido)
+                self.anemometro(self, viento)
 
-    def luxometro(self, xbee, mensajeRecibido):
+    def luxometro(self, valorLuxes):
 
-        valorLuxes = xbee.frame2adcvalue(mensajeRecibido)
 
         luxes = (10 * pow((valorLuxes - 284.62) / 69.22)) / 0.092903
         if (luxes < 50 and controlPaneles.ABIERTO == True):
@@ -60,20 +64,20 @@ class controlSensores:
         if (luxes > 50 and controlPaneles.ABIERTO == False):
             controlPaneles.sacarPaneles(controlPaneles)
 
-    def anemometro(self, xbee, mensajeRecibido):
+    def anemometro(self, viento):
 
-        viento = xbee.frame2adcvalue(mensajeRecibido)
+
 
         if (viento < self.VELOCIDAD_VIENTO_MAXIMA and controlPaneles.ABIERTO == False):
             controlPaneles.sacarPaneles(controlPaneles)
         if (viento > self.VELOCIDAD_VIENTO_MAXIMA and controlPaneles.ABIERTO == True):
             controlPaneles.sacarPaneles(controlPaneles)
 
-    def temperatura(self, xbee, mensajeRecibido):
-
-        valorTemperatura = xbee.frame2adcvalue(mensajeRecibido) / self.mV_A_Grados
+    def temperatura(self, valorTemperatura):
 
         if (valorTemperatura < self.TEMPERATURA_MINIMA):
             print("Subir persianas", valorTemperatura)
         if (valorTemperatura > self.TEMPERATURA_MAXIMA):
             print("Bajar persianas", valorTemperatura)
+        else:
+            print("La temperatura es adecuada,", valorTemperatura)
